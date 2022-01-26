d7f2a937eed84f4f08e04121ca4a887042b5627a
wjddngus1023 <wjddngus102@naver.com>
2022-01-26 16:56:05 +0900

    first commit

---
+++ src/dbCRUD.go
@@ -0,0 +1,162 @@
+package main
+
+import (
+	"database/sql"
+	"log"
+	"net/http"
+	"text/template"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+type Employee struct {
+	Id   int
+	Name string
+	City string
+}
+
+func dbConn() (db *sql.DB) {
+	dbDriver := "mysql"
+	dbUser := "root"
+	dbPw := "1234"
+	dbName := "mydb"
+	db, err := sql.Open(dbDriver, dbUser+":"+dbPw+"@/"+dbName)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
+}
+
+var temp = template.Must(template.ParseGlob("form/*"))
+
+func Index(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	selDB, err := db.Query("SELECT * FROM mytable ORDER BY Id DESC")
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Employee{}
+	res := []Employee{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+		res = append(res, emp)
+	}
+	temp.ExecuteTemplate(w, "Index", res)
+	defer db.Close()
+}
+
+func Show(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM mytable WHERE Id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Employee{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+	}
+	temp.ExecuteTemplate(w, "Show", emp)
+	defer db.Close()
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Edit(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	nId := r.URL.Query().Get("id")
+	selDB, err := db.Query("SELECT * FROM mytable WHERE id=?", nId)
+	if err != nil {
+		panic(err.Error())
+	}
+	emp := Employee{}
+	for selDB.Next() {
+		var id int
+		var name, city string
+		err = selDB.Scan(&id, &name, &city)
+		if err != nil {
+			panic(err.Error())
+		}
+		emp.Id = id
+		emp.Name = name
+		emp.City = city
+	}
+	temp.ExecuteTemplate(w, "Edit", emp)
+	defer db.Close()
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		insForm, err := db.Prepare("INSERT INTO mytable(Name, City) VALUES(?,?)")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, city)
+		log.Println("INSERT: Name: " + name + " | City: " + city)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func Update(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		city := r.FormValue("city")
+		id := r.FormValue("uid")
+		insForm, err := db.Prepare("UPDATE mytable SET Name=?, City=? WHERE Id=?")
+		if err != nil {
+			panic(err.Error())
+		}
+		insForm.Exec(name, city, id)
+		log.Println("UPDATE: Name: " + name + " | City: " + city + " | Id : " + id)
+	}
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func Delete(w http.ResponseWriter, r *http.Request) {
+	db := dbConn()
+	emp := r.URL.Query().Get("id")
+	delForm, err := db.Prepare("DELETE FROM mytable WHERE Id=?")
+	if err != nil {
+		panic(err.Error())
+	}
+	delForm.Exec(emp)
+	log.Println("DELETE")
+	defer db.Close()
+	http.Redirect(w, r, "/", 301)
+}
+
+func main() {
+	log.Println("Server is Running on : http://localhost:8080")
+	http.HandleFunc("/", Index)
+	http.HandleFunc("/show", Show)
+	http.HandleFunc("/new", New)
+	http.HandleFunc("/edit", Edit)
+	http.HandleFunc("/insert", Insert)
+	http.HandleFunc("/update", Update)
+	http.HandleFunc("/delete", Delete)
+	http.ListenAndServe(":8080", nil)
+}
